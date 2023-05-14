47a7aba17a09a6cb3c4f7d69c19137857ac62380
fares <faresaad74@gmail.com>
2023-05-14 14:04:20 +0300

    validation functions

--- game.py
+++ game.py
@@ -1,12 +1,32 @@
 from board import Board
 import time
 import random
+import math
 
 # GAME LINK
 # http://kevinshannon.com/connect4/
 
-#fares add da as a test
+EMPTY = 0
 
+
+def is_all_full(lst):
+    return all(x != EMPTY for x in lst)
+
+
+# Checks if the board is full before any move
+def is_Board_full(board):
+    for i in range(6):
+        if not is_all_full(board[i]):
+            return False
+    return True
+
+
+# checks if a column is full
+def Valid_move(board, col):
+    for i in range(5, 0, -1):
+        if board[i][col] == EMPTY:
+            return True
+    return False
 
 
 def main():
@@ -25,6 +45,7 @@ def main():
         # Insert here the action you want to perform based on the output of the algorithm
         # You can use the following function to select a column
         random_column = random.randint(0, 6)
+
         board.select_column(random_column)
 
         time.sleep(2)
