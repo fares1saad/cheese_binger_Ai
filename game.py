f82bcfcfb08b0e32c0460af7916873b56d44ab75
fesu <88581941+fares1saad@users.noreply.github.com>
2023-05-19 17:29:31 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,124 +1,96 @@
-from board import Board
-import time
-import random
-import numpy as np
+from PIL import ImageGrab
+import pyautogui
 
-# GAME LINK
-# http://kevinshannon.com/connect4/
+# YOU MAY NEED TO CHANGE THESE VALUES BASED ON YOUR SCREEN SIZE
+LEFT = 570
+TOP = 200
+RIGHT = 1350
+BOTTOM = 875
 
 EMPTY = 0
+RED = 1
+BLUE = 2
 
 
-PLAYER_PIECE = 2
-AI_PIECE = 1
-ROW_COUNT = 6
-COLUMN_COUNT = 7
-WINDOW_LENGTH = 4
-EMPTY=0
+class Board:
+    def __init__(self) -> None:
+        self.board = [[EMPTY for _ in range(7)] for _ in range(6)]
 
+    @staticmethod
+    def print_grid(grid):
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == EMPTY:
+                    print("*", end=" \t")
+                elif grid[i][j] == RED:
+                    print("R", end=" \t")
+                elif grid[i][j] == BLUE:
+                    print("B", end=" \t")
+            print("\n")
 
+    @staticmethod
+    def _convert_grid_to_color(grid):
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == (255, 255, 255):
+                    grid[i][j] = EMPTY
+                elif grid[i][j][0] > 200:
+                    grid[i][j] = RED
+                elif grid[i][j][0] > 40:
+                    grid[i][j] = BLUE
+        return grid
 
-def evaluate_window(window, piece):
-    score = 0
-    opp_piece = PLAYER_PIECE
-    if piece == PLAYER_PIECE:
-        opp_piece = AI_PIECE
+    @staticmethod
+    def _get_grid_coordinates():
+        startCord = (50, 55)
+        cordArr = []
+        for i in range(0, 7):
+            for j in range(0, 6):
+                x = startCord[0] + i * 115
+                y = startCord[1] + j * 112
+                cordArr.append((x, y))
+        return cordArr
 
-    if window.count(piece) == 4:
-        score += 10000
-    elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-        score += 5
-    elif window.count(piece) == 2 and window.count(EMPTY) == 2:
-        score += 2
+    @staticmethod
+    def _transpose_grid(grid):
+        return [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
 
-    if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
-        score -= 4
+    @staticmethod
+    def _capture_image():
+        image = ImageGrab.grab()
+        croppedImage = image.crop((LEFT, TOP, RIGHT, BOTTOM))
+        return croppedImage
 
-    return score
+    def _convert_image_to_grid(self, image):
+        pixels = [[] for _ in range(7)]
+        i = 0
+        for index, cord in enumerate(self._get_grid_coordinates()):
+            pixel = image.getpixel(cord)
+            if index % 6 == 0 and index != 0:
+                i += 1
+            pixels[i].append(pixel)
+        return pixels
 
+    def _get_grid(self):
+        cropped_image = self._capture_image()
+        pixels = self._convert_image_to_grid(cropped_image)
+        # cropped_image.show()
+        grid = self._transpose_grid(pixels)
+        return grid
 
-def score_position(board, piece): # Pass board as numpy Array
-    score = 0
+    def _check_if_game_end(self, grid):
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == EMPTY and self.board[i][j] != EMPTY:
+                    return True
+        return False
 
-    ## Score center column
-    center_array = [int(i) for i in list(board[:, COLUMN_COUNT // 2])]
-    center_count = center_array.count(piece)
-    score += center_count * 3
+    def get_game_grid(self):
+        game_grid = self._get_grid()
+        new_grid = self._convert_grid_to_color(game_grid)
+        is_game_end = self._check_if_game_end(new_grid)
+        self.board = new_grid
+        return self.board, is_game_end
 
-    ## Score Horizontal
-    for r in range(ROW_COUNT):
-        row_array = [int(i) for i in list(board[r, :])]
-        for c in range(COLUMN_COUNT - 3):
-            window = row_array[c:c + WINDOW_LENGTH]
-            score += evaluate_window(window, piece)
-
-    ## Score Vertical
-    for c in range(COLUMN_COUNT):
-        col_array = [int(i) for i in list(board[:, c])]
-        for r in range(ROW_COUNT - 3):
-            window = col_array[r:r + WINDOW_LENGTH]
-            score += evaluate_window(window, piece)
-
-    ## Score posiive sloped diagonal
-    for r in range(ROW_COUNT - 3):
-        for c in range(COLUMN_COUNT - 3):
-            window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
-            score += evaluate_window(window, piece)
-
-    for r in range(ROW_COUNT - 3):
-        for c in range(COLUMN_COUNT - 3):
-            window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
-            score += evaluate_window(window, piece)
-
-    return score
-
-
-
-def is_all_full(lst):
-    return all(x != EMPTY for x in lst)
-
-
-# Checks if the board is full before any move
-def is_Board_full(board):
-    for i in range(6):
-        if not is_all_full(board[i]):
-            return False
-    return True
-
-
-# checks if a column is full
-def Valid_move(board, col):
-    for i in range(5, 0, -1):
-        if board[i][col] == EMPTY:
-            return True
-    return False
-
-
-def main():
-    board = Board()
-
-    time.sleep(2)
-    game_end = False
-    while not game_end:
-        (game_board, game_end) = board.get_game_grid()
-
-        # FOR DEBUG PURPOSES
-        board.print_grid(game_board)
-
-        # YOUR CODE GOES HERE
-        #arr = np.array(game_board)
-
-        #score = score_position(arr, 1)
-        print(game_board)
-
-        # Insert here the action you want to perform based on the output of the algorithm
-        # You can use the following function to select a column
-        random_column = random.randint(0, 6)
-
-        board.select_column(random_column)
-
-        time.sleep(2)
-
-
-if __name__ == "__main__":
-    main()
+    def select_column(self, column):
+        pyautogui.click(LEFT + self._get_grid_coordinates()[column * 6][0], TOP)
